91a94aff701b83d07bccfcf0c304838b9348965f
Haad Bhutta <haadbhutta@gmail.com>
2024-12-07 19:40:09 -0800

    Figuring out proper way to handle fetch() in client and server

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,80 +1,37 @@
-// Request the server to get the repoCount
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-async function requestRepoCount() {
-  const result = await fetch("http://localhost:3000/repoCount");
+async function requestRepoNames() {
+  const result = await fetch("http://localhost:3000/repoName");
   if (!result.ok) {
-    if (!localStorage.getItem("repoCount")) {
-      return null;
-    }
-  } else {
-    const json = await result.json();
-    localStorage.setItem("repoCount", JSON.stringify(json["repoCount"]));
-    localStorage.setItem("repoNames", JSON.stringify(json["repoNames"]));
+    return null;
   }
+  const json = await result.json();
+  return json;
 }
 
 async function requestCommitCount(repoName) {
-  const result = await fetch(
-    `http://localhost:3000/commitCount?repoName=${repoName}`
-  );
+  const result = await fetch(`http://localhost:3000/commitCount?repoName=${repoName}`);
   if (!result.ok) {
-    if (!localStorage.getItem("commitCount")) {
-      return null;
-    }
-  } else {
-    const json = await result.json();
-    localStorage.setItem("commitCount", JSON.stringify(json["commitCount"]));
+    return null;
   }
-  return Number(JSON.parse(localStorage.getItem("commitCount")));
+  const json = await result.json();
+  return json;
 }
 
-requestRepoCount();
-/*
-const repoNames = JSON.parse(localStorage.getItem("repoNames"));
-let commitCounts = [];
+let repoNames = null;
+(async () => {
+  let repoNamesResolution = await requestRepoNames(); // Wait for the promise to resolve
+  repoNames = JSON.parse(repoNamesResolution); // Parse the resolved promise
+});
+console.log(repoNames)
 
-async function storeCommitCount(repoName) {
-  try {
-    let commitCount = await requestCommitCount(repoName);
-    if (repoName == "undefined") {
-      throw new Error("repoName is undefined");
-    }
-  } catch (error) {
-    console.error(error);
-  }
-  commitCounts.push(commitCount);
-}
 
-for (let i = 0; i < repoNames.length; i++) {
-  storeCommitCount(repoNames[i]);
-}
 
-console.log(commitCounts);
-localStorage.setItem(
-  "totalCommitCount",
-  JSON.stringify(
-    commitCounts.reduce((acc, next) => {
-      return acc + next;
-    }, 0)
-  )
-);
-*/
-console.log(`repo count: ${JSON.parse(localStorage.getItem("repoCount"))}`);
 
-// When the window has loaded,...
+// let commitCountResult = requestCommitCount();
+// let commitCount = JSON.parse(commitCountResult);
+
 window.onload = () => {
-  let repoCount = Number(JSON.parse(localStorage.getItem("repoCount")));
-  let totalCommitCount = Number(JSON.parse(localStorage.getItem("totalCommitCount")));
-  
-  if (document.getElementById("git-stats")) {
+  if (document.getElementById("git-stats"))
     document.getElementById(
       "git-stats"
-    ).innerHTML = `Haad has made ${totalCommitCount} commits in ${repoCount} repositories since August, 2022`;
-  }
+    ).innerHTML = `Haad has made ${-1} commits in ${repoNames.length} repositories since August, 2022`;
 };
-
-// document.getElementById("git").innerHTML = `Haad has made X commits in ${repoCount} repositories since August, 2022`
--- server.js
+++ server.js
@@ -1,123 +1,52 @@
 import path from "path";
 import express from "express";
 import "dotenv/config.js";
-import { Octokit } from "octokit";
+
 const __dirname = path.resolve();
 const app = express();
-
-const octokit = new Octokit({
-  userAgent: "hbhutta",
-  auth: process.env.TOKEN,
-});
 
 app.use(express.static(__dirname + "/public"));
 
 function logger(req, res, next) {
-  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  console.log(`${req.ip} sent a ${req.method} request to ${req.path}`);
   next();
 }
 app.use(logger);
 
-/**
- * Use sessionStorage to store JSON payload for big API calls
- * to avoid making redundant calls and getting the same information.
- *
- * Have a function that runs only once when the server starts,
- * and retrieves the necessary information.
- *
- * This was we only make API calls when necessary
- *
- * Chain fetch() calls by passing response from the previous call to the next call
- *
- * https://api.github.com/users/hbutta/repos
- * https://api.github.com/repos/hbhutta/xe-mri/commits
- */
-
-/**
- * We need to retrieve repo names on refresh since
- * a new repo could be made at any time.
- *
- * We also need to recount the number of commits in
- * all repos upon refresh as this can change any time.
- *
- *
- * With the above scheme, we make R + 1 API calls
- * every time the user refreshes the index view,
- * where R is the number of repositories
- *
- * But what about live reload?
- *
- * How about we add some kind of listener
- * to our chosen web entities (the API URLS)
- * and if there is any change in their content,
- * we update displayed commit count, regardless
- * of whether or not the user refreshes the website.
- *
- * This is how it should be... because the user
- * may not refresh the website for, say,
- * five whole minutes, and in that span,
- * there could be ten new commits to any
- * repository.
- *
- * How does client-side and server-side rendering relate to this?
- */
-// https://api.github.com/users/hbhutta/repos
-async function getRepoCount() {
+async function getRepoNames() {
   try {
-    const result = await fetch("https://api.github.com/users/hbhutta/repos", {
-      method: "GET",
-    });
+    const result = await fetch("https://api.github.com/users/hbhutta/repos");
+    if (!result.ok) {
+      throw new Error(`Unable to get repoNames, status code: ${result.status}`);
+    }
     const json = await result.json();
-    console.log(json);
-    let repoCountJSON = {
-      repoCount: json.length,
+    let repoNamesJSON = {
       repoNames: json.map((element) => element["name"]),
     };
-    return repoCountJSON;
+    return repoNamesJSON;
   } catch (error) {
-    console.error(`Unable to get repoCount`);
+    console.error(error);
     return null;
   }
 }
 
-// async function getRepoCount() {
-//   try {
-//     const result = await octokit.request("GET /re", {
-//       owner: "hbhutta",
-//       headers: {
-//         'X-GitHub-Api-Version': '2022-11-28'
-//       }
-//     })
-//     const json = await result.json();
-//     console.log(json);
-//     let repoCountJSON = {
-//       repoCount: json.length,
-//       repoNames: json.map((element) => element["name"]),
-//     };
-//     return repoCountJSON;
-//   } catch (error) {
-//     console.error(`Unable to get repoCount`);
-//     return null;
-//   }
-// }
-//
-
 async function getCommitCount(repoName) {
   try {
-    console.log(`Counting commits in ${repoName}...`);
-
-    const response = await fetch(
+    const result = await fetch(
       `https://api.github.com/repos/hbhutta/${repoName}/commits`
     );
-
-    const json = await response.json();
+    if (!result.ok) {
+      throw new Error(
+        `Unable to get commitCount, status code: ${result.status}`
+      );
+    }
+    const json = await result.json();
     let commitCountJSON = {
       commitCount: json.length,
     };
-    console.log(`${repoName} has ${commitCountJSON["commitCount"]} commits`);
     return commitCountJSON;
   } catch (error) {
-    console.error(`Unable to get commitCount`);
+    console.error(error);
     return null;
   }
 }
@@ -131,30 +60,15 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/home/index.html", options);
 });
 
-app.get(
-  "/commitCount",
-  async (req, res, next) => {
-    const repoName = req.query.repoName;
-    let commitCountJSON = await getCommitCount(repoName);
-    res.locals.commitCountJSON = commitCountJSON;
-    next();
-  },
-  (req, res) => {
-    res.send(res.locals.commitCountJSON);
-  }
-);
+app.get("/commitCount", async (req, res) => {
+  let commitCountJSON = await getCommitCount(req.query.repoName);
+  res.send(commitCountJSON);
+});
 
-app.get(
-  "/repoCount",
-  async (req, res, next) => {
-    let repoCountJSON = await getRepoCount();
-    res.locals.repoCountJSON = repoCountJSON;
-    next();
-  },
-  (req, res) => {
-    res.send(res.locals.repoCountJSON);
-  }
-);
+app.get("/repoNames", async (req, res) => {
+  let repoNamesJSON = await getRepoNames();
+  res.send(repoNamesJSON);
+});
 
 // Development
 const PORT = process.env.PORT || 3000;
