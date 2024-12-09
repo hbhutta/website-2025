b94fbd25f84e31428aacac585d251aa6cbc27d86
Haad Bhutta <haadbhutta@gmail.com>
2024-12-08 17:47:19 -0800

    Added endpoint to find number of insertions in a repo

--- server.js
+++ server.js
@@ -18,18 +18,19 @@ function logger(req, res, next) {
 }
 app.use(logger);
 
-async function getSomething(URL) {
-  let response = await fetch(URL);
-  let value = await response.json();
+// async function getSomething(URL) {
+//   let response = await fetch(URL);
+//   let value = await response.json();
+//   return value;
+// }
+
+async function getRepoNames() {
+  let response = await fetch("https://api.github.com/users/hbhutta/repos");
+  let value = await response.json()
   return value;
 }
 
-async function getRepoNames() {
-  return getSomething("https://api.github.com/users/hbhutta/repos");
-}
-
-async function getCommitCount(repoName) {
-  console.log(repoName);
+async function getCommits(repoName) {
   let response = await octokit.request("GET /repos/{owner}/{repo}/commits", {
     owner: "hbhutta",
     repo: repoName,
@@ -37,6 +38,21 @@ async function getCommitCount(repoName) {
       "X-GitHub-Api-Version": "2022-11-28",
     },
   });
+  return response;
+}
+
+async function getInsertionCount(repoName, commitRef) {
+  let response = await octokit.request(
+    "GET /repos/{owner}/{repo}/commits/{ref}",
+    {
+      owner: "hbhutta",
+      repo: repoName,
+      ref: commitRef,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    }
+  );
   return response;
 }
 
@@ -48,25 +64,23 @@ app.get("/", (req, res) => {
   };
   res.sendFile(__dirname + "/views/home/index.html", options);
 });
-
 app.get("/repoNames", async (req, res) => {
   let repoNamesJSON = await getRepoNames();
-  console.log(repoNamesJSON);
   res.send(repoNamesJSON);
 });
 
-app.get("/commitCount", async (req, res) => {
-  let commitCountJSON = await getCommitCount(req.query.repoName);
-  console.log(commitCountJSON);
-  console.log(req.query.repoName);
-
-  if (commitCountJSON == null) {
-    res.send(null);
-  }
-  res.send(commitCountJSON);
+app.get("/commits", async (req, res) => {
+  let commitsJSON = await getCommits(req.query.repoName);
+  res.send(commitsJSON);
 });
 
-
+app.get("/insertionCount", async (req, res) => {
+  let insertionCountJSON = await getInsertionCount(
+    req.query.repoName,
+    req.query.commitRef
+  );
+  res.send(insertionCountJSON);
+});
 
 // Development
 const PORT = process.env.PORT || 3000;
